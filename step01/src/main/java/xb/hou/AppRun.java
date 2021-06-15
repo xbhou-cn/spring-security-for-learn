367add46e23f860acbdf581ab8995ac286b3e1cb
xbhou-cn <47892921+xbhou-cn@users.noreply.github.com>
2021-06-15 22:27:48 +0800

    Initial commit

---
+++ step01/src/main/java/xb/hou/AppRun.java
@@ -0,0 +1,18 @@
+package xb.hou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @title: AppRun
+ * @Author xbhou
+ * @Date: 2021-06-15 22:16
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class AppRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class, args);
+    }
+}
